2ca73ca7022c93f9b85b787736f237d5ec278a2a
krishnakanth590 <krishnakanth.590@gmail.com>
2015-08-19 18:21:22 -0500

    Test
    test

--- src/com/pkg/twitter/Authorization.java
+++ src/com/pkg/twitter/Authorization.java
@@ -5,10 +5,10 @@ package com.pkg.twitter;
  * to collect tweet information */
 public class Authorization {
 
-	private final static String CONSUMER_KEY = "CYK0a04c5lzVSGa74JdQH0lpJ";
-	private final static String CONSUMER_KEY_SECRET = "PJFC3DzXr7rhR90hgeR3rDX7wPwWAyWWNW70GrPXm0XzCJyqPL";
-	private final static String AUTHORIZATION_TOKEN = "3025651503-SO8yqeqG3a8TCgl7doSu3BdoxZjVT4oePRzdpVU";
-	private final static String AUTHORIZATION_SECRET = "ZJL1c1iRElBDMnou0Ltw7N9ti3ye7soXW0H2rfabO2n5R";
+	private final static String CONSUMER_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+	private final static String CONSUMER_KEY_SECRET = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+	private final static String AUTHORIZATION_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+	private final static String AUTHORIZATION_SECRET = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
 
 	public static String getConsumerKey() {
 		System.out.println("Sharing CONSUMER_KEY");
